3cfa31b19f1af5e762e36f3280fcf827eeb34759
DaniJaVa <danisjv@gmail.com>
2017-03-08 09:59:48 -0600

    Agregado Mapa

--- app/src/main/java/com/example/danis/pulpomatic/MainActivity.java
+++ app/src/main/java/com/example/danis/pulpomatic/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.danis.pulpomatic;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        MapFragment mapFragment =
+                (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+
     }
 }
+
