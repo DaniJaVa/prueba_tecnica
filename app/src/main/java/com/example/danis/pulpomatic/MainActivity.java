0e47650aa5a6bed2391143f4e587648d193ff4eb
DaniJaVa <danisjv@gmail.com>
2017-03-08 22:20:14 -0600

    Se agrego search bar y mejoras de visualizacion

--- app/src/main/java/com/example/danis/pulpomatic/MainActivity.java
+++ app/src/main/java/com/example/danis/pulpomatic/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.danis.pulpomatic;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
@@ -9,6 +12,9 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -17,7 +23,12 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
 
@@ -26,6 +37,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     double lat;
     LatLng latlng;
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    Address calle;
+    Marker marker;
+    MarkerOptions miUbicacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +53,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        //Obteniendo dirección actual
         LocationManager locationManager = (LocationManager)getSystemService(this.LOCATION_SERVICE);
         Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         lon = location.getLongitude();
         lat = location.getLatitude();
         latlng = new LatLng(lat,lon);
+
+        try {
+            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+            List<Address> list = geocoder.getFromLocation(lat, lon, 1);
+
+            calle = list.get(0);
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+        miUbicacion = new MarkerOptions().position(latlng).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                .title("Dirección")
+                .snippet(calle.getAddressLine(0));
+
 
     }
 
@@ -58,10 +87,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
                 mGoogleMap.setMyLocationEnabled(true);
                 mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 16));
-                mGoogleMap.addMarker(new MarkerOptions()
-                .position(latlng).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
-                .title("Soi io")
-                .snippet("Kien io?"));
+                mGoogleMap.addMarker(miUbicacion);
             }
         } else {
             //Si es menor, no requiere pedir permisos
@@ -111,6 +137,33 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 }
                 return;
             }
+
+        }
+    }
+
+    public void onMapSearch(View view){
+        mGoogleMap.clear();
+        EditText locationSearch = (EditText)findViewById(R.id.editText);
+        String location = locationSearch.getText().toString();
+        List<Address>addressList = null;
+
+        if(location != null || !location.equals("")){
+            Geocoder geocoder = new Geocoder(this);
+            try{
+                addressList = geocoder.getFromLocationName(location, 1);
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+
+                mGoogleMap.addMarker(miUbicacion);
+                mGoogleMap.addMarker(new MarkerOptions().position(latLng).title("Direccion").snippet(address.getAddressLine(0)).draggable(true));
+                mGoogleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(locationSearch.getWindowToken(), 0);
 
         }
     }
