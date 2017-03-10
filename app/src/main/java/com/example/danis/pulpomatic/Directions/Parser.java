434b85287de129bc864de3509602cf90c99db38a
DaniJaVa <danisjv@gmail.com>
2017-03-09 23:23:05 -0600

    Agregada polyline entre mi ubicacion y el marcador

---
+++ app/src/main/java/com/example/danis/pulpomatic/Directions/Parser.java
@@ -0,0 +1,9 @@
+package com.example.danis.pulpomatic.Directions;
+
+/**
+ * Created by danis on 09/03/2017.
+ */
+
+public interface Parser {
+    public Route parse();
+}
--- app/src/main/java/com/example/danis/pulpomatic/MainActivity.java
+++ app/src/main/java/com/example/danis/pulpomatic/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.danis.pulpomatic;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -12,33 +13,40 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.akexorcist.googledirection.DirectionCallback;
+import com.akexorcist.googledirection.GoogleDirection;
+import com.akexorcist.googledirection.constant.TransportMode;
+import com.akexorcist.googledirection.model.Direction;
+import com.akexorcist.googledirection.model.Route;
+import com.akexorcist.googledirection.util.DirectionConverter;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
 
+    String serveKey = "AIzaSyDw3prMyflvWLpEekCyMjtmoY5PWaCD5lE";
     GoogleMap mGoogleMap;
     double lon;
     double lat;
-    LatLng latlng;
+    LatLng latlngActual;
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
     Address calle;
-    Marker marker;
     MarkerOptions miUbicacion;
 
     @Override
@@ -55,10 +63,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         //Obteniendo dirección actual
         LocationManager locationManager = (LocationManager)getSystemService(this.LOCATION_SERVICE);
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
         lon = location.getLongitude();
         lat = location.getLatitude();
-        latlng = new LatLng(lat,lon);
+        latlngActual = new LatLng(lat,lon);
 
         try {
             Geocoder geocoder = new Geocoder(this, Locale.getDefault());
@@ -69,10 +77,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             e.printStackTrace();
         }
 
-        miUbicacion = new MarkerOptions().position(latlng).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+        miUbicacion = new MarkerOptions().position(latlngActual).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
                 .title("Dirección")
                 .snippet(calle.getAddressLine(0));
-
 
     }
 
@@ -86,8 +93,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
 
                 mGoogleMap.setMyLocationEnabled(true);
-                mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 16));
-                mGoogleMap.addMarker(miUbicacion);
+                mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlngActual, 15));
+                //mGoogleMap.addMarker(miUbicacion);
             }
         } else {
             //Si es menor, no requiere pedir permisos
@@ -129,7 +136,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     // Permiso aceptado!!
                     if (ActivityCompat.checkSelfPermission(this,
                             Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                        mGoogleMap.setMyLocationEnabled(true);
+                        //mGoogleMap.setMyLocationEnabled(true);
                     }
                 } else {
                     //Permiso denegado!!
@@ -141,28 +148,50 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-    public void onMapSearch(View view){
+    public void onMapSearch(View view) {
         mGoogleMap.clear();
-        EditText locationSearch = (EditText)findViewById(R.id.editText);
+        EditText locationSearch = (EditText) findViewById(R.id.editText);
         String location = locationSearch.getText().toString();
-        List<Address>addressList = null;
+        List<Address> addressList = null;
 
-        if(location != null || !location.equals("")){
+        if (location != null || !location.equals("")) {
             Geocoder geocoder = new Geocoder(this);
-            try{
+            try {
                 addressList = geocoder.getFromLocationName(location, 1);
-            }catch(IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            Address address = addressList.get(0);
-            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            final Address address = addressList.get(0);
+            final LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
 
-                mGoogleMap.addMarker(miUbicacion);
-                mGoogleMap.addMarker(new MarkerOptions().position(latLng).title("Direccion").snippet(address.getAddressLine(0)).draggable(true));
-                mGoogleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            GoogleDirection.withServerKey(serveKey)
+                    .from(latlngActual)
+                    .to(latLng)
+                    .transitMode(TransportMode.WALKING)
+                    .execute(new DirectionCallback() {
+                        @Override
+                        public void onDirectionSuccess(Direction direction, String rawBody) {
+                            Log.d("Status", direction.getStatus());
 
-            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                            if (direction.isOK()) {
+                                Route route = direction.getRouteList().get(0);
+                                //mGoogleMap.addMarker(miUbicacion);
+                                mGoogleMap.addMarker(new MarkerOptions().position(latLng));
+                                mGoogleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                                ArrayList<LatLng> directionPositionList = route.getLegList().get(0).getDirectionPoint();
+                                mGoogleMap.addPolyline(DirectionConverter.createPolyline(MainActivity.this, directionPositionList, 5, Color.RED));
+
+                            }
+                        }
+
+                        @Override
+                        public void onDirectionFailure(Throwable t) {
+                            Log.e("Error", t.toString());
+                        }
+                    });
+
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(locationSearch.getWindowToken(), 0);
 
         }
